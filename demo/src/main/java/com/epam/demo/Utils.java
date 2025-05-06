43444ffe01e27084d9f307d486dd1194764d2816
Igor Bugaiov <bugayov.igor@nixsolutions.com>
2025-05-06 18:19:11 +0200

    fix number validation, experimental stuff

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,10 +5,11 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         try {
-            return !args.isEmpty() && args.stream()
+            return args != null && !args.isEmpty() && args.stream()
                     .allMatch(n ->
-                            n != null && !n.isEmpty() && !n.equals(" ")
-                                    && (Integer.parseInt(n) > 0
+                            n != null && !n.trim().isEmpty()
+                                    && (!n.trim().matches("-?\\d*\\.?\\d+")
+                                    || Integer.parseInt(n) > 0
                                     || Double.parseDouble(n) > 0
                                     || Long.parseLong(n) > 0));
         } catch (NumberFormatException e) {
