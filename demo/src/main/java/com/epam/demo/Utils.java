91ad53de147b523955841a99d42958ce47821f1d
Igor Bugaiov <bugayov.igor@nixsolutions.com>
2025-05-06 18:21:49 +0200

    reverted experimental stuff to a prev working solution

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,9 +7,8 @@ public class Utils {
         try {
             return args != null && !args.isEmpty() && args.stream()
                     .allMatch(n ->
-                            n != null && !n.trim().isEmpty()
-                                    && (n.trim().matches("-?\\d*\\.?\\d+")
-                                    || Integer.parseInt(n) > 0
+                            n != null && !n.isEmpty() && !n.equals(" ")
+                                    && (Integer.parseInt(n) > 0
                                     || Double.parseDouble(n) > 0
                                     || Long.parseLong(n) > 0));
         } catch (NumberFormatException e) {
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,9 +3,8 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            return str != null && !str.trim().isEmpty()
-                    && (str.trim().matches("-?\\d*\\.?\\d+")
-                    || Integer.parseInt(str) > 0
+            return str != null && !str.isEmpty() && !str.equals(" ")
+                    && (Integer.parseInt(str) > 0
                     || Double.parseDouble(str) > 0
                     || Long.parseLong(str) > 0);
         } catch (NumberFormatException e) {
