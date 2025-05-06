8c201e135c260aad9b1a5a90b3195c5f0f6cd6f0
Igor Bugaiov <bugayov.igor@nixsolutions.com>
2025-05-06 17:58:22 +0200

    fix number validation, add more complex detection

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return !args.isEmpty() && args.stream()
-                .allMatch(n ->
-                        n != null && !n.isEmpty() && Integer.parseInt(n) >= 0);
+        try {
+            return !args.isEmpty() && args.stream()
+                    .allMatch(n ->
+                            n != null && !n.isEmpty() && !n.equals(" ")
+                                    && Integer.parseInt(n) > 0
+                                    && Double.parseDouble(n) > 0
+                                    && Long.parseLong(n) > 0);
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return str != null && !str.isEmpty() && Integer.parseInt(str) >= 0;
+        try {
+            return str != null && !str.isEmpty() && !str.equals(" ")
+                    && Integer.parseInt(str) > 0
+                    && Double.parseDouble(str) > 0
+                    && Long.parseLong(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
+
 }
